55ba277836b7d88b42a4a5aa2c60231315d15b0c
Astra <abhaypratapsinghwd@gmail.com>
2025-08-17 10:34:58 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,77 @@
+import { siteConfig } from "@/config/site";
+import type { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
+import { Toaster } from "sonner";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(siteConfig.url),
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: ["Community", "LinkedIn"],
+  authors: [
+    {
+      name: "SinghAstra",
+      url: "https://github.com/SinghAstra",
+    },
+  ],
+  creator: "SinghAstra",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: "/assets/landing.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: ["/assets/landing.png"],
+    creator: "@singhastra",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="antialiased min-h-screen bg-background ">
+        <NextTopLoader color="hsl(var(--primary))" showSpinner={false} />
+        {children}
+        <Toaster
+          theme="dark"
+          toastOptions={{
+            style: {
+              fontFamily: "Space Grotesk, monospace",
+              background: "hsl(var(--muted) / 0.2)",
+              backdropFilter: "blur(20px)",
+              color: "hsl(var(--foreground))",
+              border: "1px solid hsl(var(--border))",
+              letterSpacing: "0.05em",
+              fontSize: ".95rem",
+              borderRadius: "var(--radius)",
+            },
+          }}
+        />
+      </body>
+    </html>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/interfaces/site";
+
+export const siteConfig: SiteConfig = {
+  name: "Motion",
+  description: "Theory of Motion & its Application",
+  url: "https://notes-motion.vercel.app",
+  links: {
+    twitter: "https://x.com/XSinghAstra",
+    github: "https://github.com/SinghAstra",
+    githubRepo: "https://github.com/SinghAstra/motion",
+  },
+};
